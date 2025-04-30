fe4f63301b90c4d9f2dffc9bb754740492c52b93
aleksaelezovic <aleksaelezovic@gmail.com>
2025-04-30 14:41:06 +0200

    Allow nquads pipeline

--- services/kMiningService.js
+++ services/kMiningService.js
@@ -13,9 +13,18 @@ exports.defineProcessingPipelineId = async req => {
     const kmining_csv_pipeline_id = req.user.config.find(
         item => item.option === 'kmining_csv_pipeline_id'
     )?.value;
+    const kmining_nq_pipeline_id = req.user.config.find(
+        item => item.option === 'kmining_nq_pipeline_id'
+    )?.value;
 
     if (req.file.mimetype === 'application/ld+json') {
         return 'simple_json_to_jsonld';
+    }
+    if (
+        req.file.mimetype === 'application/n-quads' ||
+        req.file.originalname.endsWith('.nq')
+    ) {
+        return kmining_nq_pipeline_id;
     }
     if (req.file.mimetype === 'application/json') {
         return kmining_json_pipeline_id;
